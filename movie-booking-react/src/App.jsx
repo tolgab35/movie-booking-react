46b457d637d7062a0b41bb95f4b02dd9ad3cdd4c
Tolga Boz <tolgaboz350@gmail.com>
2025-11-14 18:40:48 +0300

    refactor: remove debug code and unused UI elements

--- movie-booking-react/src/App.jsx
+++ movie-booking-react/src/App.jsx
@@ -4,7 +4,6 @@ import { Home } from "./pages/Home";
 function App() {
   return (
     <div>
-      <h1>Movie Booking</h1>
       <Home />
     </div>
   );
--- movie-booking-react/src/services/moviesService.js
+++ movie-booking-react/src/services/moviesService.js
@@ -3,7 +3,6 @@ import { api } from "./axiosInstance";
 export const moviesService = {
   getGenres: async () => {
     const res = await api.get("/genre/movie/list");
-    console.log(res.data);
     return res.data.genres;
   },
 };
